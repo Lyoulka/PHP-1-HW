fd088e4b036267a275f77ea24935e870341e4190
Lyoulka <Lyoulka@mail.ru>
2019-02-11 10:25:04 +0400

    Change

---
+++ lesson1/Ex4/list.php
@@ -0,0 +1,11 @@
+<?php
+ $pdo = new PDO('mysql:host=localhost;dbname=', '', '');
+	$stmt = $pdo->prepare('SELECT * FROM authors');
+	$stmt->execute();
+	foreach ($stmt as $row) {
+	    echo $row['id']; echo '_';
+		echo $row['name']; echo '_';
+		echo $row['phone']; echo ';_';
+	}
+	
+?>
